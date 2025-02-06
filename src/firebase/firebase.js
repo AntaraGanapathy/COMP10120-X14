f209e3dd9ac9276921bc59d1db9457453c22d027
Antara Ganapathy <antaraganapathy@gmail.com>
2025-02-06 11:58:45 +0000

    set-up auth

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJG4VRB6YZB7BR0bHxmTddyd2tWY7jeeg",
+  authDomain: "dish-it-out-df6c0.firebaseapp.com",
+  projectId: "dish-it-out-df6c0",
+  storageBucket: "dish-it-out-df6c0.firebasestorage.app",
+  messagingSenderId: "756254290949",
+  appId: "1:756254290949:web:2cc566a802f4d3b61d986c",
+  measurementId: "G-V8XL41MTSL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, auth };
