1dce610f0bde6619f14c762ce5005bf0e7f2c0cb
Pranav Kiruthik Mani <pranavkiruthikmani@gmail.com>
2025-02-13 14:53:05 +0000

    Running test code

--- src/components/roomid/index.jsx
+++ src/components/roomid/index.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {database, ref, set, push} from '../../firebase/firebase';
+import firebase from 'firebase/compat/app';
 
-function addRoom(roomid){
 
-    firebase.database()
-
+function saveValue(roomid, name){
+    set(ref('rooms/${roomid}'), {
+        username : name,
+        exampletext : "test"
+    })
+    .then(() => console.log("Data written succesfully"))
+    .catch((error) => console.error("Error"));
+    
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getDatabase, ref, set, push} from "firebase/database";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +23,6 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-const database = firebase.database();
+const database = getDatabase(app);
 
-export { app, auth };
+export { app, auth, database, ref, set, push };
